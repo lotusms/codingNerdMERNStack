55d7d8554edc7365878affa016abfba1a31cb186
DESKTOP-01P0GAI\lotus <lotusms@outlook.com>
2019-06-03 13:18:34 -0400

    changed

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const DashboardActions = () => {
+const DashboardActions = ({ user: _id }) => {
   return (
     <div className='dash-buttons'>
+      <Link to={`/profile/${_id}`} className='btn btn-light'>
+        <i className='fas fa-user-circle text-primary' /> See Profile
+      </Link>
       <Link to='/edit-profile' className='btn btn-light'>
-        <i className='fas fa-user-circle text-primary' /> Edit Profile
+        <i className='fas fa-user-edit text-primary' /> Edit Profile
       </Link>
       <Link to='/add-experience' className='btn btn-light'>
         <i className='fab fa-black-tie text-primary' /> Add Experience
@@ -15,6 +19,10 @@ const DashboardActions = () => {
       </Link>
     </div>
   );
+};
+
+DashboardActions.propTypes = {
+  profile: PropTypes.object.isRequired
 };
 
 export default DashboardActions;
