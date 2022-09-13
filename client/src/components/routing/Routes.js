201e3f795b0834b0e0fd55bde9a5360cb3d2b0c9
Luis Silva <lotusms@outlook.com>
2022-09-13 10:59:52 -0400

    set main root route

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -20,17 +20,17 @@ const Routes = () => {
     <section className='container'>
       <Alert />
       <Switch>
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/profiles' component={Profiles} />
-        <Route exact path='/profile/:id' component={Profile} />
-        <PrivateRoute exact path='/dashboard' component={Dashboard} />
-        <PrivateRoute exact path='/create-profile' component={CreateProfile} />
-        <PrivateRoute exact path='/edit-profile' component={EditProfile} />
-        <PrivateRoute exact path='/add-experience' component={AddExperience} />
-        <PrivateRoute exact path='/add-education' component={AddEducation} />
-        <PrivateRoute exact path='/posts' component={Posts} />
-        <PrivateRoute exact path='/posts/:id' component={Post} />
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+        <Route path='/profiles' component={Profiles} />
+        <Route path='/profile/:id' component={Profile} />
+        <PrivateRoute path='/dashboard' component={Dashboard} />
+        <PrivateRoute path='/create-profile' component={CreateProfile} />
+        <PrivateRoute path='/edit-profile' component={EditProfile} />
+        <PrivateRoute path='/add-experience' component={AddExperience} />
+        <PrivateRoute path='/add-education' component={AddEducation} />
+        <PrivateRoute path='/posts' component={Posts} />
+        <PrivateRoute path='/posts/:id' component={Post} />
         <Route component={NotFound} />
       </Switch>
     </section>
